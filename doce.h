c939629e02da6e5aa7f4c85c8da4b9acfedc1c18
LedApalachee <ledapalachee@gmail.com>
2021-08-01 01:48:25 +0700

    small changes

--- doce.h
+++ doce.h
@@ -40,7 +40,7 @@ typedef struct
 } Screen;
 
 
-void init_screen(Screen *scr, int sx, int sy);
+void init_screen(Screen *scr, int size_x, int size_y);
 void free_screen(Screen *scr);
 
 void draw_screen(Screen *scr);
@@ -49,8 +49,8 @@ void clear_screen(Screen *scr);
 Pixel* getpixel(Screen *scr, int x, int y);
 void putpixel(Screen *scr, Pixel pix, int x, int y); // sets pixel in matrix and draws it on a screen
 void clearpixel(Screen *scr, int x, int y);
-void setpixel(Screen *scr, Pixel pix, int x, int y); // only sets pixel in matrix
-void drawpixel(Screen *scr, int x, int y);
+void setpixel(Screen *scr, Pixel pix, int x, int y); // only sets pixel in matrix without drawing
+void drawpixel(Screen *scr, int x, int y); // draws an already existing pixel
 
 void _goto(int x, int y); // moves console cursor
 
